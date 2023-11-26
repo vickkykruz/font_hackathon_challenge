f62c068a6512f828079b1aacbedf96b7f56d3909
VickkyProgrammer <73972088+vickkykruz@users.noreply.github.com>
2023-11-26 15:34:01 -0800

    done

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
+// let accordion = document.querySelector('.accordionBox'),
+//     accordionItem = document.querySelectorAll('.accordionItem');
+//     accordionItem.forEach(elem => {
+//       elem.addEventListener('click', function(e) {
+//         e.srcElement.parentElement.classList.toggle('active');
+//       });
+//     });
+
 let accordion = document.querySelector('.accordionBox'),
-    accordionItem = document.querySelectorAll('.accordionItem');
-    accordionItem.forEach(elem => {
-      elem.addEventListener('click', function(e) {
-        e.srcElement.parentElement.classList.toggle('active');
-      });
+  accordionItem = document.querySelectorAll('.accordionItem');
+
+accordionItem.forEach(elem => {
+  elem.addEventListener('click', function(e) {
+    // Remove 'active' class from all accordion items
+    accordionItem.forEach(item => {
+      if (item !== e.currentTarget) {
+        item.classList.remove('active');
+      }
     });
+
+    // Toggle 'active' class on the clicked accordion item
+    e.currentTarget.classList.toggle('active');
+  });
+});
